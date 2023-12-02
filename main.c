d9b6ee9df114eb77e97b70bee620beb6fb58e492
Fernando Moretti <130860907+Fernando-Moretti@users.noreply.github.com>
2023-12-02 00:06:13 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,35 +1,31 @@
-#pragma config FOSC = XT        // Oscillator Selection bits (XT oscillator)
+
+#pragma config FOSC = XT     // Oscillator Selection bits (XT oscillator)
 #pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
 #pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
-#pragma config BOREN = OFF       // Brown-out Reset Enable bit (BOR enabled)
-#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = OFF         // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3/PGM pin has PGM function; low-voltage programming enabled)
 #pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
-#pragma config WRT = OFF       // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
 #pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
 
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
 
-
-#include <xc.h>            // Inclusao da biblioteca do compilador
-
-
-
-#define _XTAL_FREQ 4000000 // fosc 4Mhz neste caso
-
-#define LED PORTCbits.RC3  // Somente para facilitar a leitura do programa
-
-int valor; // var para guardar valor da conversao
-
+#include <xc.h>
+#include <pic16f877a.h>
+#define _XTAL_FREQ 20000000
 #define INC_BTN !PORTDbits.RD0  // Increment button
 #define DEC_BTN !PORTDbits.RD1 // Decrement button 
 #define OK_BTN !PORTDbits.RD2 // botao que o usuario aperta qnd for confirmar a temperatura requerida
-
+#define LED PORTCbits.RC3  // Somente para facilitar a leitura do programa
 #define OUT_UNI PORTC // Unity display
 #define OUT_DEZ PORTB // Tens display  
-
 #define FLAG_TEMPORIZER flagsbits.f0 // Temporizer flag
 
 
-float calcular_temperatura(float valor);
+unsigned int digital_value=0; 
+unsigned int new_value=0;
+
 
 void aumentar();
 
@@ -41,146 +37,82 @@ void ligar_aquecedor();
 
 void desligar_aquecedor();
 
-unsigned int ADC_Read(unsigned int channel);
-
-
-
-
-
-
-
-
-
-
-
-
-//*** subrotina para tratar a interrrup??o *************************************
-void __interrupt() TrataInt(void)
+void ADC_Init() 
 {
-  if (PIR1bits.ADIF)  //foi a interrupcao de final de conversao AD?
-     {
-        PIR1bits.ADIF = 0; //reseta o flag da interrupcao
-        valor = (ADRESH << 8) | ADRESL;;    // Passa valores convertido do reg para a variavel
-        PORTB = (unsigned char)valor;    // Coloca os valores convertido no port B em binario
+    ADRESL = 0x00;
+    ADRESH = 0x00;
 
-        //testa valor da conversao em formato decimal
-        //if (valor == 55)
-        //    LED = 1;
-        //else
-        //    LED = 0;
+    ADCON0bits.ADCS1 = 1;
+    ADCON0bits.ADCS0 = 1;
+    ADCON0bits.CHS2 = 1;
+    ADCON0bits.CHS1 = 0;
+    ADCON0bits.CHS0 = 0;
+    ADCON0bits.GO_nDONE = 0; //ONLY TURN ON WHEN READING
+    ADCON0bits.ADON = 1;
+
+    ADCON1bits.ADFM = 1;
+    ADCON1bits.ADCS2 = 0;
+    ADCON1bits.ADFM = 1;
+    ADCON1bits.PCFG3 = 0;
+    ADCON1bits.PCFG2 = 0;
+    ADCON1bits.PCFG1 = 0;
+    ADCON1bits.PCFG0 = 0;
+
+    PIR1bits.ADIF = 0;
+}
+
+void Int_Init() 
+{
+    INTCONbits.GIE = 1;
+    INTCONbits.PEIE = 1;
+    PIE1bits.ADIE = 1;
+}
+
+void ADC_Read() 
+{
+    ADCON0bits.GO_nDONE = 1; //INITIALIZE A/D CONVERTION
+}
+
+void __interrupt() TrataInt(void)
+{    
+  if (ADIF)  //foi a interrup??o de final de convers?o AD?
+     {  
+        PIR1bits.ADIF = 0; //reseta o flag da interrup??o 
+        digital_value=((ADRESH<<8)+ADRESL);
+        new_value = digital_value/2;
+        
+        
   }
   return;
 }
 
-void main(void)
-{
-       //** Configurando interrupcoes ***********************************
-   INTCONbits.GIE  = 1;       // Habiliita a int global
-   INTCONbits.PEIE = 1;       // Habilita a int dos perifericos
-   PIE1bits.ADIE   = 1;       // Habilita int do AD
-   //** Inicializacoes ************************************************
-   TRISB = 0b00000000;       // Configura pinos de entrada(1)e saida (0)
-   TRISC = 0b00000000;       // Configura pinos de entrada(1)e saida (0)
-   TRISA = 0xFF;       // Configura pinos de entrada(1)e saida (0)
-   
-   OPTION_REGbits.nRBPU = 0; // Ativa resistores de pull-ups
+void main(void) {
+    // configura  es e setup
 
-   //*** Configura o conversor analogico/digital*******************************
-   // PORTA AN0
-   // Configura Somente com AN0 como entrada analogica
-   // Ver tabela no datasheet (pg.128)
-   // Vdd/Vss como Vref+/Vref-
-   //**************************************************************************
-   ADCON1bits.PCFG0 = 0;     // Configura as entradas analogicas
-   ADCON1bits.PCFG1 = 1;     // Configura as entradas analogicas
-   ADCON1bits.PCFG2 = 1;     // Configura as entradas analogicas
-   ADCON1bits.PCFG3 = 1;     // Configura as entradas analogicas
-   ADCON0bits.CHS = 0;
-   
-   // Define o clock de conversao
-   ADCON0bits.ADCS0 = 0  ;   // Confirmando default Fosc/2
-   ADCON0bits.ADCS1 = 0  ;   // Confirmando default Fosc/2
-
-   ADCON1bits.ADFM  = 0   ;  // Default 0. Pra mostra que pegar os dados em
-                             // 8 bits no ADRESH, pois ests justifica a esquerda
-                             // Passando pra 1 pode pegar os dados em 10 bits
-                             // usando os dois registradores ADRESHL justificado
-                             // a direita
-
-   // Inicializa registradores do AD
-   ADRESL = 0x00;            // Valor inicial da conversao em 0
-   ADRESH = 0x00;
-
-   ADCON0bits.ADON = 1;      // Liga AD
-
-   // **** inicializando saidas
-   LED = 0;   // Apaga LED
-   PORTB = 0; // Apaga LEDS da conversao
-
-   ADCON0bits.ADON = 1;  //
-
-   //Seleciona canal de entrada 0 como entrada analogica
-   ADCON0bits.CHS0 = 0; // Configura canal 0 como entrada analogica
-   ADCON0bits.CHS1 = 0; // Configura canal 0 como entrada analogica
-   ADCON0bits.CHS2 = 0; // Configura canal 0 como entrada analogica
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   //****************************************************************************************
-   
-   // configurações e setup
-
-  OUT_UNI = 0;
-  OUT_DEZ = 0;
+    OUT_UNI = 0;
+    OUT_DEZ = 0;
   
-   TRISBbits.TRISB7 = 0; // RB7 como saída 
-  ADCON1bits.PCFG0 = 1; // RA0 configurado como I/O digital
-   ADCON1bits.PCFG1 = 0; // RA1 configurado como entrada analógica
-   
-   ADCON0 = 0x81;
-   ADCON1 = 010000000;
-   
-   ADCON0bits.CHS0 = 0; 
-    ADCON0bits.CHS1 = 1; // Canal AN1 selecionado
-    
-    
+    OPTION_REGbits.nRBPU = 0; // Ativa resistores de pull-ups
+    PORTA = 0xFF;
+    PORTD = 0x00;
+    TRISC = 0x00;
+    PORTC = 0x00;
+    TRISB = 0b00000000;       // Configura pinos de entrada(1)e saida (0)
+    TRISC = 0b00000000;       // Configura pinos de entrada(1)e saida (0)
+    TRISA = 0xFF;       // Configura pinos de entrada(1)e saida (0)
+    TRISBbits.TRISB7 = 0; // RB7 como sa da 
     float temperatura_do_usuario = 0;
     float temperatura_requerida;
     float valor_do_sensor;
-     float temperatura_do_sensor;
+    float temperatura_do_sensor;
    __delay_us(20);
-   ADCON0bits.GO = 1; // Inicia a conversão
-   
-   
-   while (1)   //loop infinito
-   {
-       //ADRESH = 0;
-       //ADRESL = 0;
- 
-       // while(ADCON0bits.GO == 1) {}; // Espera até a conversão acabar
-      // ADCON0bits.GO = 1;  // INICIA CONVERS?O
-     //  __delay_us(10);     // Tempo de conversao
-     //  __delay_ms(1);      // Tempo de conversao
-       
-       
-       valor_do_sensor = ADRESH; // lê o valor convertido
-       temperatura_do_sensor = calcular_temperatura(valor_do_sensor);
-       
-       
-       // mostrar a temperatura que o usuario setou no display
-       // comparar a temperatura setado pelo user com a do sensor
-       // === se for menor que a dor sensor -> ligar aquecedor
-       // se a temp do sensor for maior que a user -> desligar sensor
-       
-       if(INC_BTN){
+    ADC_Init();
+    Int_Init();
+    while (1) 
+    {
+        ADC_Read();
+        __delay_ms(1);
+        if(INC_BTN){
            aumentar(); // aumenta o valor no display apenas
            temperatura_do_usuario++;
        }
@@ -193,13 +125,13 @@ void main(void)
            
            if(temperatura_do_sensor < temperatura_requerida){
                while(temperatura_do_sensor < temperatura_requerida){
-                   
+                   ADC_Read();
                ligar_aquecedor();
                temporizer();
                
                
-               valor_do_sensor = ADRESH; // lê o valor convertido
-               temperatura_do_sensor = calcular_temperatura(valor_do_sensor);
+               valor_do_sensor = new_value; // l  o valor convertido
+               temperatura_do_sensor = new_value;
                 
                temporizer();
        
@@ -214,49 +146,13 @@ void main(void)
                
            }
        }
-       
-       
-       
+    
     }
+
+
+
+    return;
 }
-
-
-
-
-
-float calcular_temperatura(float valor){
-    float temperatura;
-    temperatura = (valor * 5 * 100) / 1023; // LM35 - 10mV/oC, tensão alimentação 5V
-    return temperatura;
-    
-    
-    /*
-     Onde:
-
-5 = tensão alimentação em Volts
-100 = fator escala, 10mV -> 0,01V (100x)
-1023 = resolução conversor AD
-Exibir ou usar o valor de temperatura para o controle desejado.
-Assim lê-se a informação do sensor de temperatura na entrada analógica AN1 e converte para temperatura em oC.
-     */
-}
-
- /*
-  * 
-  * float calcTemperatura(float Tensao){
-    float Temperatura = (Tensao - 1.6111)/(-0.0218);    // Equação normalizada da temperatura
-    valor = Temperatura;	
-    return valor;
-  * 
-  * 
-}
-  * 
-  * */
-
-/* 
-Função que calcula temperatura  baseada na linearidade da resistencia do sensor que foi usado como referencia em aula.
-*/
-
 
 void aumentar(){
     if(OUT_UNI == 9){
